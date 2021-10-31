eb07a8ae975d06603208249edd490641a962c1fa
admin <ataewisma@gmail.com>
2021-11-01 00:13:17 +0300

    add class Tour.java

--- Server/src/main/java/controller/main/Main.java
+++ Server/src/main/java/controller/main/Main.java
@@ -5,12 +5,6 @@ import com.example.model.tour.Tour;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client();
-        client.setFIO("qwerty");
-        System.out.println(client.getFIO());
 
-        Tour tour = new Tour();
-        tour.setTourName("asdf");
-        System.out.print(tour.getTourName());
     }
 }
