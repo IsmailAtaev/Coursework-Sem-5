348253c47a1406aa25f6eb13ef0f2387928b86f8
admin <ataewisma@gmail.com>
2021-11-01 00:07:58 +0300

    add interface IInformation.java

---
+++ Client/src/main/java/com/example/model/iinformation/IInformation.java
@@ -0,0 +1,11 @@
+package com.example.model.iinformation;
+
+import java.io.Serializable;
+
+/**
+ * @author Atayev Ismayyl
+ * */
+
+public interface IInformation extends Serializable {
+    void getToString();
+}
